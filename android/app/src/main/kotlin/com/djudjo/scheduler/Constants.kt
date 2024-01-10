3222b171c1a3e0c9559b6c03a4e996080a8f3b40
DANIS <danis.preldzic@sqa-consulting.com>
2024-01-11 00:13:33 +0100

    fix

--- android/app/src/main/kotlin/com/djudjo/scheduler/Constants.kt
+++ android/app/src/main/kotlin/com/djudjo/scheduler/Constants.kt
@@ -10,14 +10,14 @@ class Constants {
             when (BuildConfig.FLAVOR) {
                 "dev" -> {
                     HASH_KEY = "04e309788f5e811ef3767af6ce885fcb"
-                    PLATFORM_PREFIX = "https://tower-test."
+                    PLATFORM_PREFIX = ""
                 }
                 "qa" -> {
                     HASH_KEY = ""
                 }
                 "prod" -> {
                     HASH_KEY = "d91ddb6e94c9289071c528693ff76841"
-                    PLATFORM_PREFIX = "https://tower."
+                    PLATFORM_PREFIX = ""
                 }
             }
         }
