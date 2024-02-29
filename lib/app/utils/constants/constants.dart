e275f3354c70509ee5f37244a56a59c3edc7b268
DANIS <danis.preldzic@sqa-consulting.com>
2024-02-29 15:01:48 +0100

    imperative gradle changes dotenv implemented

--- lib/app/utils/constants/constants.dart
+++ lib/app/utils/constants/constants.dart
@@ -1,11 +1,3 @@
-class FirebaseConstants {
-  static const String API_KEY = 'AIzaSyARlC4_ic69tcY8J7sgha9psk_Zmh_H1QY';
-  static const String APP_ID = '1:592885880330:android:9bcc79ff7b4a7d14ebb1f3';
-  static const String MESSAGING_SENDER_ID = '592885880330';
-  static const String PROJECT_ID = 'djudjoinkscheduler';
-  static const String STORAGE_BUCKET = 'djudjoinkscheduler.appspot.com';
-}
-
 class FontConstants {
   static const String POPPINS_FONT = 'Poppins';
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,10 @@
 import 'package:djudjo_scheduler/app/utils/constants/constants.dart';
+import 'package:djudjo_scheduler/env/env.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/MyApp.dart';
 import 'app/locator.dart';
@@ -11,10 +13,10 @@ import 'config/flavor_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await storagePrefs.init();
   await firebaseInitialize();
   FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-  // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   FlavorConfig(flavor: Flavor.DEV, values: FlavorValues(appName: AppName.APP_NAME));
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,12 +28,12 @@ void main() async {
 
 Future<void> firebaseInitialize() async {
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: FirebaseConstants.API_KEY,
-      appId: FirebaseConstants.APP_ID,
-      messagingSenderId: FirebaseConstants.MESSAGING_SENDER_ID,
-      projectId: FirebaseConstants.PROJECT_ID,
-      storageBucket: FirebaseConstants.STORAGE_BUCKET,
+    options: FirebaseOptions(
+      apiKey: Env.firebaseApiKey!,
+      appId: Env.firebaseAppId!,
+      messagingSenderId: Env.messagingSenderId!,
+      projectId: Env.firebaseProjectId!,
+      storageBucket: Env.firebaseStorageBucket!,
     ),
   ).then((FirebaseApp value) => print('Firebase Initialize ${DateTime.now().toString().split(' ')[0]}'));
 }
